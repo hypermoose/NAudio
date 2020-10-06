8528fc6bd0e35988fc691012e74b322ebe6f6b61
Gary Caldwell <gacald@microsoft.com>
2020-10-06 16:14:28 -0700

    Test program

---
+++ NAudioWpfDemo/KatoSample/KatoPlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace NAudioWpfDemo.KatoSample
+{
+    class KatoPlugin : IModule
+    {
+        private KatoViewModel viewModel;
+        private KatoView view;
+
+        public string Name => "Kato";
+
+        public UserControl UserInterface
+        {
+            get { if (view == null) CreateView(); return view; }
+        }
+
+        private void CreateView()
+        {
+            view = new KatoView();
+            viewModel = new KatoViewModel();
+            view.DataContext = viewModel;
+        }
+
+        public void Deactivate()
+        {
+            viewModel.Dispose();
+            view = null;
+        }
+    }
+}
---
+++ NAudioWpfDemo/KatoSample/KatoViewModel.cs
@@ -0,0 +1,200 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using NAudioWpfDemo.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace NAudioWpfDemo.KatoSample
+{
+    internal class KatoViewModel : ViewModelBase, IDisposable
+    {
+        private MMDevice selectedDevice;
+        private WasapiCapture capture;
+        private int sampleRate;
+        private int bitDepth;
+        private int channelCount;
+        private int sampleTypeIndex;
+        private string message;
+        private readonly SynchronizationContext synchronizationContext;
+
+        public DelegateCommand RecordCommand { get; }
+        public DelegateCommand StopCommand { get; }
+
+        private int shareModeIndex;
+
+        public KatoViewModel()
+        {
+            synchronizationContext = SynchronizationContext.Current;
+            var enumerator = new MMDeviceEnumerator();
+            CaptureDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToArray();
+            var defaultDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
+            SelectedDevice = CaptureDevices.FirstOrDefault(c => c.ID == defaultDevice.ID);
+            RecordCommand = new DelegateCommand(Record);
+            StopCommand = new DelegateCommand(Stop) { IsEnabled = false };
+        }
+
+        private void Stop()
+        {
+            capture?.StopRecording();
+        }
+
+        private void Record()
+        {
+            try
+            {
+                capture = new WasapiCapture(SelectedDevice);
+                capture.ShareMode = ShareModeIndex == 0 ? AudioClientShareMode.Shared : AudioClientShareMode.Exclusive;
+                capture.WaveFormat =
+                    SampleTypeIndex == 0 ? WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, channelCount) :
+                    new WaveFormat(sampleRate, bitDepth, channelCount);
+                capture.StartRecording();
+                capture.RecordingStopped += OnRecordingStopped;
+                capture.DataAvailable += CaptureOnDataAvailable;
+                RecordCommand.IsEnabled = false;
+                StopCommand.IsEnabled = true;
+                Message = "Recording...";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void CaptureOnDataAvailable(object sender, WaveInEventArgs waveInEventArgs)
+        {
+        }
+
+
+        void OnRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (e.Exception == null)
+                Message = "Recording Stopped";
+            else
+                Message = "Recording Error: " + e.Exception.Message;
+            capture.Dispose();
+            capture = null;
+            RecordCommand.IsEnabled = true;
+            StopCommand.IsEnabled = false;
+        }
+
+        public IEnumerable<MMDevice> CaptureDevices { get; }
+
+        public MMDevice SelectedDevice
+        {
+            get => selectedDevice;
+            set
+            {
+                if (selectedDevice != value)
+                {
+                    selectedDevice = value;
+                    OnPropertyChanged("SelectedDevice");
+                    GetDefaultRecordingFormat(value);
+                }
+            }
+        }
+
+        private void GetDefaultRecordingFormat(MMDevice value)
+        {
+            using (var c = new WasapiCapture(value))
+            {
+                SampleTypeIndex = c.WaveFormat.Encoding == WaveFormatEncoding.IeeeFloat ? 0 : 1;
+                SampleRate = c.WaveFormat.SampleRate;
+                BitDepth = c.WaveFormat.BitsPerSample;
+                ChannelCount = c.WaveFormat.Channels;
+                Message = "";
+            }
+        }
+
+        public int SampleRate
+        {
+            get => sampleRate;
+            set
+            {
+                if (sampleRate != value)
+                {
+                    sampleRate = value;
+                    OnPropertyChanged("SampleRate");
+                }
+            }
+        }
+
+        public int BitDepth
+        {
+            get => bitDepth;
+            set
+            {
+                if (bitDepth != value)
+                {
+                    bitDepth = value;
+                    OnPropertyChanged("BitDepth");
+                }
+            }
+        }
+
+        public int ChannelCount
+        {
+            get => channelCount;
+            set
+            {
+                if (channelCount != value)
+                {
+                    channelCount = value;
+                    OnPropertyChanged("ChannelCount");
+                }
+            }
+        }
+
+        public bool IsBitDepthConfigurable => SampleTypeIndex == 1;
+
+        public int SampleTypeIndex
+        {
+            get => sampleTypeIndex;
+            set
+            {
+                if (sampleTypeIndex != value)
+                {
+                    sampleTypeIndex = value;
+                    OnPropertyChanged("SampleTypeIndex");
+                    BitDepth = sampleTypeIndex == 1 ? 16 : 32;
+                    OnPropertyChanged("IsBitDepthConfigurable");
+                }
+            }
+        }
+
+        public string Message
+        {
+            get => message;
+            set
+            {
+                if (message != value)
+                {
+                    message = value;
+                    OnPropertyChanged("Message");
+                }
+            }
+        }
+
+        public int ShareModeIndex
+        {
+            get => shareModeIndex;
+            set
+            {
+                if (shareModeIndex != value)
+                {
+                    shareModeIndex = value;
+                    OnPropertyChanged("ShareModeIndex");
+                }
+            }
+        }
+
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
